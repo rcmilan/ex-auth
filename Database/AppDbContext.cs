a28c5a32219c5b54fdaf913f7cfd857bed68e081
Ricardo Milan <ricardo@intercambiodireto.com>
2022-05-01 10:32:40 -0300

    identity database

---
+++ Database/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    internal class AppDbContext : IdentityDbContext // contém as tabelas necessárias
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Database/ModuleDependency.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Database
+{
+    public static class ModuleDependency
+    {
+
+        public static IServiceCollection AddDatabaseModule(this IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(config => config.UseInMemoryDatabase("memorydb"))
+                .AddIdentity<IdentityUser, IdentityRole>(config =>
+                {
+                    config.Password.RequiredLength = 8;
+                    config.Password.RequireDigit = false;
+                    config.Password.RequireNonAlphanumeric = false;
+                    config.Password.RequireUppercase = false;
+                })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
