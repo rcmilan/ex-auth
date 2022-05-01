abb11032d51624898347354c20ee108e8b025971
Ricardo Milan <ricardo@intercambiodireto.com>
2022-05-01 10:32:58 -0300

    login e register

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 
 namespace App.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,33 +23,86 @@ namespace App.Controllers
         [Authorize]
         public IActionResult Secret()
         {
-            //var user = HttpContext.User;
+            var user = HttpContext.User;
 
             return View();
         }
 
-        public IActionResult Authenticate()
+        public IActionResult Login()
         {
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, "UserName"),
-                new Claim(ClaimTypes.Email, "user@email.com")
-            };
+            return View();
+        }
 
-            var hueClaims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, "Hue br"),
-                new Claim("hu3huehue", "brbrbr")
-            };
+        public IActionResult Register()
+        {
+            return View();
+        }
 
-            var identity = new ClaimsIdentity(claims, "identity123");
-            var hueIdentity = new ClaimsIdentity(hueClaims, "huebr identity");
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var identityUser = await _userManager.FindByNameAsync(username);
 
-            var userPrincipal = new ClaimsPrincipal(new[] { identity, hueIdentity });
+            if(identityUser != null)                
+                return await SignIn(password, identityUser);
 
-            HttpContext.SignInAsync(userPrincipal);
+            return RedirectToAction("Register");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            var identityUser = new IdentityUser(username);
+
+            var result = await _userManager.CreateAsync(identityUser, password);
+
+            if(result.Succeeded)
+                return await SignIn(password, identityUser);
 
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        private async Task<IActionResult> SignIn(string password, IdentityUser identityUser)
+        {
+            var signInResult = await _signInManager.PasswordSignInAsync(identityUser, password, isPersistent: false, lockoutOnFailure: false);
+
+            if (signInResult.Succeeded)
+            {
+                // ações após success no sign in
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //public IActionResult Authenticate()
+        //{
+        //    var claims = new List<Claim>
+        //    {
+        //        new Claim(ClaimTypes.Name, "UserName"),
+        //        new Claim(ClaimTypes.Email, "user@email.com")
+        //    };
+
+        //    var hueClaims = new List<Claim>
+        //    {
+        //        new Claim(ClaimTypes.Name, "Hue br"),
+        //        new Claim("hu3huehue", "brbrbr")
+        //    };
+
+        //    var identity = new ClaimsIdentity(claims, "identity123");
+        //    var hueIdentity = new ClaimsIdentity(hueClaims, "huebr identity");
+
+        //    var userPrincipal = new ClaimsPrincipal(new[] { identity, hueIdentity });
+
+        //    HttpContext.SignInAsync(userPrincipal);
+
+        //    return RedirectToAction("Index");
+        //}
     }
 }
